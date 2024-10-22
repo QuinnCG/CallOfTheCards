c2996ea1849de47d4650e9bd17f5735c2203317f
Quinton <quinncg.dev@gmail.com>
2024-10-22 12:43:58 -0400

    Added Witch Art

---
+++ Assets/Source/CardBehaviors/BroodingWitch.cs
@@ -0,0 +1,18 @@
+﻿namespace Quinn.CardBehaviors
+{
+	public class BroodingWitch : CardBehavior
+	{
+		private void Awake()
+		{
+			EventManager.OnCardDie += OnCardDeath;
+		}
+
+		private void OnCardDeath(Card card)
+		{
+			if (card != Card)
+			{
+				Card.SetDP(card.DP);
+			}
+		}
+	}
+}
